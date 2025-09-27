2e34a9c65446a59cbfe4c3906cd194c06e73f12a
mahdipour.e <mahdipour.e@MAHDIPOUR-SN>
2025-09-27 13:21:54 +0330

    FIRST Commit

---
+++ Fitamon.Endpoint.Api/Controllers/Bot/BotController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fitamon.Endpoint.Api.Controllers.Bot
+{
+    public class BotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
