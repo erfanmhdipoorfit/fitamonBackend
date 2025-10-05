80ba5df7980b6b5e8f997175617fd1c8c355870e
mahdipour.e <mahdipour.e@MAHDIPOUR-SN>
2025-10-05 09:53:26 +0330

    ADD:swagger config

---
+++ Fitamon.Endpoint.Api/Dtos/SwaggerDocConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Fitamon.Endpoint.Api.Dtos
+{
+    public class SwaggerDocConfig
+    {
+        public string Name { get; set; } = default!;
+        public string Title { get; set; } = default!;
+        public string Url { get; set; } = default!;
+        public string Version { get; set; } = default!;
+    }
+}
--- Fitamon.Endpoint.Api/Program.cs
+++ Fitamon.Endpoint.Api/Program.cs
@@ -1,21 +1,71 @@
+﻿using Fitamon.Endpoint.Api.Dtos;
+//using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.ComponentModel;
+using System.Reflection;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var swaggerConfig = configuration.GetSection("Swagger");
+var docs = swaggerConfig.GetSection("Docs").Get<List<SwaggerDocConfig>>();
 
 builder.Services.AddControllers();
 
-// ? ??? Swashbuckle (???? Microsoft.AspNetCore.OpenApi)
+// ✅ فقط Swashbuckle (بدون Microsoft.AspNetCore.OpenApi)
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "My API",
-        Version = "v1"
-    });
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new OpenApiInfo
+//    {
+//        Title = "My API",
+//        Version = "v1"
+//    });
 
-    // ??? ??? ???? 6.5.0+ ??? ????? ??? ?? ??? ??????
-    //c.EnableAnnotations();
+//    // فقط اگر نسخه 6.5.0+ نصب باشد، این خط کار می‌کند
+//    //c.EnableAnnotations();
+//});
+
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.CustomSchemaIds(x =>
+        x.GetCustomAttributes(false).OfType<DisplayNameAttribute>().FirstOrDefault()?.DisplayName ?? x.Name);
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+
+    if (File.Exists(xmlPath))
+    {
+        opt.IncludeXmlComments(xmlPath);
+    }
+
+    docs?.ForEach(doc =>
+        opt.SwaggerDoc(doc.Name, new OpenApiInfo { Title = doc.Title, Version = doc.Version }));
+
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
 });
 
 var app = builder.Build();
@@ -25,8 +75,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
-        c.RoutePrefix = "swagger";
+        docs?.ForEach(doc =>
+            c.SwaggerEndpoint(doc.Url, $"{doc.Title} - {doc.Version}"));
+        c.DocExpansion(DocExpansion.None);
     });
 }
 
